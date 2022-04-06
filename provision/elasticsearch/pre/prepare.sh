6a9ef8030f2dcc014c0c437850b633c5523242ef
Justin Zhang <schnell18@gmail.com>
2022-04-06 20:57:27 +0800

    Add jaeger tracing tool

--- provision/elasticsearch/pre/prepare.sh
+++ provision/elasticsearch/pre/prepare.sh
@@ -1,8 +1,16 @@
 source provision/global/libs/functions.sh
 
+# check vm.max_map_count of host to ensure it is no less than 262144
+vmMaxMapCount=$(sysctl vm.max_map_count | cut -d' ' -f3)
+if [[ $vmMaxMapCount -lt 262144 ]]; then
+    # we don't persist the setting
+    echo "Set vm.max_map_count to 262144..."
+    sudo sysctl -w vm.max_map_count=262144
+fi
+
 if [[ ! -d .state/elasticsearch/es01 ]]; then
     mkdir -p .state/elasticsearch/es01
     chmod g+rwx .state/elasticsearch/es01
-    chgrp 0 .state/elasticsearch/es01
+    sudo chgrp 0 .state/elasticsearch/es01
 fi
 
