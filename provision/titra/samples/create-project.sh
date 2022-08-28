ae1a7d3ae4fbd1e7daafc5b335b975da083e3b12
Justin Zhang <schnell18@gmail.com>
2022-08-28 23:31:42 +0800

    Add titra api call example

---
+++ provision/titra/samples/create-project.sh
@@ -0,0 +1,13 @@
+curl 'http://localhost:3000/project/create/' \
+  -H 'Content-Type: application/json; charset=UTF-8' \
+  -H 'Accept: */*' \
+  -H 'Authorization: Token: ribxxexkaxcPRoEeN' \
+  -d'{
+    "name": "资源重构一期",
+    "description": "资源重构一期",
+    "color": "#009688",
+    "customer": "PPL",
+    "rate": 100,
+    "budget": 50
+}
+'
