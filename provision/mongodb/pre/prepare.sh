a35cf5ca66c69cb2c8b2613309b15f47fe5cee85
Justin Zhang <schnell18@gmail.com>
2022-09-30 21:26:23 +0800

    Temp fix node binary search path error

--- provision/mongodb/pre/prepare.sh
+++ provision/mongodb/pre/prepare.sh
@@ -2,6 +2,7 @@ source provision/global/libs/functions.sh
 
 if [[ ! -d .state/mongodb/data ]]; then
     mkdir -p .state/mongodb/data
+    sudo chmod o+rwx .state/mongodb/data
 fi
 
 # if [[ ! -d .state/mongodb/logs ]]; then
