7749f1f4de520a461a5cf0bb96b4d269ae03c2ae
Justin Zhang <schnell18@gmail.com>
2022-09-30 22:12:11 +0800

    Stablize elasticsearch provision

--- provision/elasticsearch/pre/prepare.sh
+++ provision/elasticsearch/pre/prepare.sh
@@ -1,16 +1,26 @@
 source provision/global/libs/functions.sh
 
 function setup_macos {
-    podman machine ssh sed -e '$a * soft memlock -1' \
-                           -e '$a * hard memlock -1' \
-                           -e '$a * soft nproc 4096' \
-                           -e '$a * hard nproc 8192' \
-                           /etc/security/limits.d/eslaticsearch.conf
-    podman machine ssh sysctl -w vm.max_map_count=262144
+    cat<<EOF | podman machine ssh --username root localenv
+cat<<EOT > /etc/security/limits.d/elasticsearch.conf
+* soft memlock -1
+* hard memlock -1
+* soft nproc 4096
+* hard nproc 8192
+EOT
+sysctl -w vm.max_map_count=262144 > /dev/null
+getent passwd 1000 > /dev/null
+if [[ $? -ne 0 ]]; then
+ useradd -u 1000 esrunner
+fi
+chown -R 1000 $(pwd)/.state/elasticsearch/es01
+EOF
+#chown -R esrunner /Users/user/localenv/.state/elasticsearch/es01
+
 }
 
 function setup_linux {
-    sudo cat<<EOF > /etc/security/limits.d/eslaticsearch.conf
+    sudo cat<<EOF > /etc/security/limits.d/elasticsearch.conf
 * soft memlock -1
 * hard memlock -1
 * soft nproc 4096
@@ -19,19 +29,13 @@ EOF
     sudo sysctl -w vm.max_map_count=262144
 }
 
+if [[ ! -d .state/elasticsearch/es01 ]]; then
+    mkdir -p .state/elasticsearch/es01
+fi
+
 case `uname` in
     Darwin) setup_macos ;;
     Linux) setup_linux ;;
     *) echo ""
 esac
-
-sudo cat<<EOF > /etc/sysctl.d/eslaticsearch.conf
-vm.max_map_count = 262144
-EOF
-
-if [[ ! -d .state/elasticsearch/es01 ]]; then
-    mkdir -p .state/elasticsearch/es01
-    # chmod g+rwx .state/elasticsearch/es01
-    # chgrp 0 .state/elasticsearch/es01
-fi
 
