495dfc715afb638baaa035ff3ec860f5c6c8fb9f
Justin Zhang <schnell18@gmail.com>
2025-04-21 15:33:50 +0800

    Remove titra and wekan
    - Remove seldom used titra and wekan
    
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- .infra/wekan/provision/pre/prepare.ps1
+++
@@ -1,6 +0,0 @@
-# Import functions from the global library
-. ".\.infra\global\libs\functions.ps1"
-
-if (-not (Test-Path ".state\wekan\data")) {
-    New-Item -Path ".state\wekan\data" -ItemType Directory -Force | Out-Null
-}
--- .infra/wekan/provision/pre/prepare.sh
+++
@@ -1,6 +0,0 @@
-source .infra/global/libs/functions.sh
-
-if [[ ! -d .state/wekan/data ]]; then
-    mkdir -p .state/wekan/data
-fi
-
