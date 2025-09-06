c30aeab32acdb5e5423a02592aacf8f883fa1087
Justin Zhang <schnell18@gmail.com>
2025-09-06 14:24:54 +1200

    Implement #5 support NATS with NUI
    - Fix nui health check failure
    - replace busybox nc with openbsd netcat in nui docker image
    
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

---
+++ Containerfiles/nui/build.sh
@@ -0,0 +1,28 @@
+NUI_VERSION=$(jq -r '.nuiVersion' version.json)
+REV=$(jq -r '.build' version.json)
+
+REGISTRY=docker.io
+USER=schnell18
+IMAGE_NAME=nui
+IMAGE_TAG=${NUI_VERSION}-${REV}
+MANIFEST=${IMAGE_NAME}
+
+# remove local manifest from previous build so that
+# we don't have previous versions to pollute the new build
+podman manifest exists ${MANIFEST}
+if [[ $? -eq 0 ]]; then
+    podman manifest rm ${MANIFEST}
+fi
+
+podman manifest create ${MANIFEST}
+
+# --platform linux/amd64,linux/arm64/v8 \
+podman build \
+    --jobs 2 \
+    --platform linux/amd64 \
+    --manifest ${MANIFEST} \
+    --build-arg NUI_VERSION=${NUI_VERSION} \
+    --tag $REGISTRY/$USER/$IMAGE_NAME:$IMAGE_TAG \
+    .
+
+# podman manifest push --all ${MANIFEST} docker://$REGISTRY/$USER/$IMAGE_NAME:$IMAGE_TAG
